3c9466e2ca5fdec1833885e3e77475af57d2f120
Indrajit-ISIOffice <rs_math1902@isibang.ac.in>
2024-03-15 12:59:54 +0530

    indrajit's cv ready

--- indrajit.py
+++ indrajit.py
@@ -5,9 +5,10 @@
 class Indrajit:
     first_name = "Indrajit"
     family_name = "Ghosh"
-    address = ["8th Mile Mysore Road, Pin-560 059", "Bangalore, India"]
+    address = ["8th Mile Mysore Road, RVCE Post", "Bangalore 560 059, India"]
     mobile = "(+91) 9564 957618"
     email = "indrajitghosh912@gmail.com"
+    homepage = "https://indrajitghosh.onrender.com/"
 
     education = [
         {
@@ -71,22 +72,198 @@ class Indrajit:
 
     contri_talks = [
         {
-            'title': "Young Mathematicians in Operator Algebras",
-            'date': "Feb 27, 2024",
-            'institute': "Indian Statistical Institute Delhi",
-            'country': "Delhi, India",
+            'title': "Workshop on Completely Positive Maps",
+            'date': "Jan 15 -- Apr 29, 2024",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
             'venue': "Stat-Math Unit, ISI",
-            'website': {'url': "https://sites.google.com/view/ymoa/home", 'text': ''}
+            'website': {'url': "https://indrajitghosh.onrender.com/teaching/isibc/intro_to_cp_even_2024.html", 'url_text': 'Click here'},
+            'abstract': {}
         },
-    ]
-
-    conferences = [
         {
             'title': "Young Mathematicians in Operator Algebras",
             'date': "Feb 27, 2024",
             'institute': "Indian Statistical Institute Delhi",
             'country': "Delhi, India",
             'venue': "Stat-Math Unit, ISI",
-            'website': {'url': "https://sites.google.com/view/ymoa/home", 'text': ''}
+            'website': {'url': "https://sites.google.com/view/ymoa/home", 'url_text': ''},
+            'abstract': {'url':"https://sites.google.com/view/ymoa/talks?authuser=0", 'url_text': "Click here"}
+        },
+        {
+            'title': "International Conference on Spectral and Approximation Theory",
+            'date': "Nov 29, 2023",
+            'institute': "Cochin University of Science and Technology",
+            'country': "Kerala, India",
+            'venue': "Department of Mathematics, CUSAT",
+            'website': {'url': "https://sites.google.com/view/icsat-23/", 'url_text': ''},
+            'abstract': {'url':"https://sites.google.com/view/icsat-23/abstracts?authuser=0", 'url_text': "Click here"}
+        },
+    ]
+
+    conferences = [
+        {
+            'title': "Young Mathematicians in Operator Algebras",
+            'date': "Feb 26 -- Mar 02, 2024",
+            'institute': "Indian Statistical Institute Delhi",
+            'country': "Delhi, India",
+            'venue': "Stat-Math Unit, ISI",
+            'website': {'url': "https://sites.google.com/view/ymoa/home", 'url_text': ''}
+        },
+        {
+            'title': "International Conference on Spectral and Approximation Theory",
+            'date': "Nov 27 -- 30, 2023",
+            'institute': "Cochin University of Science and Technology",
+            'country': "Kerala, India",
+            'venue': "Department of Mathematics, CUSAT",
+            'website': {'url': "https://sites.google.com/view/icsat-23/", 'url_text': ''},
+        },
+        {
+            'title': "Conference on Functional Analysis and Related Topics-2023",
+            'date': "Feb 21 -- 25, 2023",
+            'institute': "Indian Institute of Technology Bombay, India",
+            'country': "Bombay, India",
+            'venue': "Victor Menezes Convention Centre(VMCC), IIT Bombay",
+            'website': {'url': "https://sites.google.com/view/cfart-2023-iitb/", 'url_text': ''}
+        },
+        {
+            'title': "Tomita-Takesaki Modular Theory",
+            'date': "Jan 18 -- Apr 26, 2023",
+            'institute': "Indian Statistical Institute, Bangalore, India",
+            'country': "Bangalore, India",
+            'venue': "Stat-Math Unit, ISI",
+            'website': {'url': "https://www.isibang.ac.in/~soumyashant/teaching/MODULAR_THEORY-EVEN2023/CourseWebpage.html", 'url_text': 'Click here'},
+        },
+        {
+            'title': "Ergodic Theory and Dynamical Systems",
+            'date': "Dec 05 -- 16, 2022",
+            'institute': "International Centre for Theoretical Sciences",
+            'country': "Bangalore, India",
+            'venue': "Ramanujan Lecture Hall (ICTS) and Online",
+            'website': {'url': "https://www.icts.res.in/program/ETDS2022", 'url_text': ''},
+        },
+        {
+            'title': "Baby Steps Beyond the Horizon",
+            'date': "Aug 29 - Sep 02, 2022",
+            'institute': "Institute of Mathematics Polish Academy of Sciences",
+            'country': "Warszawa, Poland",
+            'venue': "Online",
+            'website': {'url': "https://www.impan.pl/en/activities/banach-center/conferences/22-babysteps", 'url_text': ''},
+        },
+        {
+            'title': "A Conference on Ergodic Theory and von-Neumann Algebra",
+            'date': "Aug 04 -- 06, 2022",
+            'institute': "National Institute of Science Education and Research",
+            'country': "Odisha, India",
+            'venue': "Online",
+            'website': {'url': "https://www.niser.ac.in/etvna", 'url_text': ''},
+        },
+        {
+            'title': "IFCAM - Mathematical Aspects of Quantum Mechanics",
+            'date': "Jun 01 -- 12, 2022",
+            'institute': "Indian Institute of Science Bangalore",
+            'country': "Bangalore, India",
+            'venue': "Math Dept., IISc",
+            'website': {'url': "https://ifcam.sciencesconf.org/", 'url_text': ''},
+        },
+        {
+            'title': "Advanced Instructional School - Advanced Linear Algebra",
+            'date': "May 09 -- 28, 2022",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'venue': "Stat-Math Unit, ISI",
+            'website': {'url': "https://www.atmschools.org/school/2022/AIS/ala", 'url_text': ''},
+        },
+        {
+            'title': "Quantum Probability and Infinite Dimensional Analysis",
+            'date': "Jan 17 -- 20, 2022",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'venue': "Stat-Math Unit, ISI",
+            'website': {'url': "https://www.isibang.ac.in/~statmath/conferences/QPIDA-2022.html", 'url_text': ''},
+        },
+        {
+            'title': "IWM Mini Course on Approximate Solutions of Operator Equations",
+            'date': "Nov 2021",
+            'institute': "Indian Women and Mathematics",
+            'country': "India",
+            'venue': "Online",
+            'website': {'url': "https://drive.google.com/file/d/1FYd-6j\_hwOMfc2Z9NIQiv2ZqSnF859J4/view", 'url_text': ''},
+        },
+        {
+            'title': "Online Workshop on Algebraic Number Theory",
+            'date': "Sep 2022",
+            'institute': "Assam University",
+            'country': "Assam, India",
+            'venue': "Online",
+            'website': {},
+        },
+    ]
+
+    teaching_experience = [
+        {
+            'role': "Teaching Assistant",
+            'year': "Jan 2024 -- Present",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'course': "A Course in Harmonic Analysis (B. Math III)",
+            'instructor': "Soumyashant Nayak",
+            'webpage': "https://indrajitghosh.onrender.com/teaching/isibc/course_harmonic_even2024.html"
+        },
+        {
+            'role': "Teaching Assistant",
+            'year': "Aug 01 -- Nov 25, 2023",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'course': "Analysis of Several Variables (B. Math II)",
+            'instructor': "Mathew Joseph",
+            'webpage': "https://indrajitghosh.onrender.com/teaching/isibc/anal_several_vars_odd_2023.html"
+        },
+        {
+            'role': "Teaching Assistant",
+            'year': "Aug 2022 -- Nov 2022",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'course': "Complex Analysis I (B. Math III)",
+            'instructor': "Mathew Joseph",
+            'webpage': "https://indrajitghosh.onrender.com/teaching/isibc/comp_anal_odd_sem_2022.html"
+        },
+        {
+            'role': "Teaching Assistant",
+            'year': "Jan 2022 -- Apr 2022",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'course': "Functional Analysis (M. Math I)",
+            'instructor': "Soumyashant Nayak",
+            'webpage': "https://indrajitghosh.onrender.com/teaching/isibc/func_anal_even_sem_2021.html"
+        },
+        {
+            'role': "Teaching Assistant",
+            'year': "Sep 21 -- Dec 23, 2021",
+            'institute': "Indian Statistical Institute Bangalore",
+            'country': "Bangalore, India",
+            'course': " Optimization (B. Math II)",
+            'instructor': "Soumyashant Nayak",
+            'webpage': "https://indrajitghosh.onrender.com/teaching/isibc/optimization_odd_sem_2021.html"
+        },
+    ]
+
+    references = [
+        {
+            'name': "Dr. Soumyashant Nayak",
+            'email': "soumyashant@isibang.ac.com",
+            'designation': "Assistant Professor",
+            'address': "Office A-14, Stat-Math Unit, Indian Statistical Institute Bangalore"
+        },
+        {
+            'name': "Dr. Nisith Chandra Das",
+            'email': "dasnisith@gmail.com",
+            'designation': "Associate Professor",
+            'address': "Department Of Mathematics, Barasat Govt. College, West Bengal, India"
+        },
+        {
+            'name': "Dr. Atasi Deb Ray",
+            'email': "adrpm@caluniv.ac.in",
+            'designation': "Associate Professor",
+            'address': "Department Of Pure Mathematics, University of Calcutta"
         },
     ]
--- indrajit_cv.py
+++ indrajit_cv.py
@@ -11,6 +11,16 @@ from pathlib import Path
 import os, sys
 from indrajit import Indrajit
 
+def _get_weblink_tex(url:str, url_text:str=None):
+    """
+    This functions accepts an `url` (e.g- "https://google.com/") and 
+    an optional `url_text` (e.g- "Click here").
+    And returns a str 
+        r"\texttt{\href{ <url> }{ <url_text> }}}" 
+    """
+    url_text = url_text if url_text else url
+    return f"\\texttt{{\\href{{{url}}}{{{url_text}}}}}"
+
 
 class IndrajitCV(CurriculumVitae):
 
@@ -22,7 +32,7 @@ class IndrajitCV(CurriculumVitae):
             mobile=Indrajit.mobile,
             email=Indrajit.email,
             style="banking",
-            font_size=12,
+            font_size=10,
             photo_height=70,
             photo_thickness=0.9,
         )
@@ -46,7 +56,7 @@ class IndrajitCV(CurriculumVitae):
         self.add_section(title="Publication")
         for pub in Indrajit.publication:
             preprint = (
-                '(\\emph{Pre-print})'
+                '(\\emph{Pre-print}) '
                 if pub['preprint'] == 1
                 else ''
             )
@@ -58,7 +68,7 @@ class IndrajitCV(CurriculumVitae):
 
             tex_line = (
                 preprint +
-                r" \emph{"
+                r"\emph{"
                 + pub['title']
                 + "}"
                 + r"{" + collaborators + r"}, "
@@ -79,26 +89,102 @@ class IndrajitCV(CurriculumVitae):
         # Contributed Talks
         self.add_section(title="Contributed Talks")
         for talk in Indrajit.contri_talks:
-            desc = (
-                r"\cvlistitem{"
-                + f"Venue: {talk['venue']}" + "}"
-                r"\cvlistitem{"
-                + r"Website: \texttt{" 
-                + r"\href{"
-                + talk['website']['text'] 
-                + "}{"
-                + talk['website']['url'] + "}}"
+            talk_desc = (
+                "\n"
+                + r"\cvlistitem{"
+                + f"Venue: {talk['venue']}" 
+                + "}"
+                + "\n"
+                + r"\cvlistitem{Website: "
+                + _get_weblink_tex(url=talk['website']['url'], url_text=talk['website']['url_text'])
                 + "}"
             )
+            if  'abstract' in talk.keys() and talk['abstract']:
+                talk_desc += (
+                    "\n"
+                    + r"\cvlistitem{Abstract: "
+                    + _get_weblink_tex(url=talk['abstract']['url'], url_text=talk['abstract']['url_text'])
+                    + "}"
+                    +"\n"
+                )
             self.add_cventry(
                 years=talk['date'],
                 degree_or_job_title=talk['institute'],
                 institution_or_employer=talk['title'],
                 localization=talk['country'],
-                description=desc
+                description=talk_desc
             )
-    
+        
+        # Teaching Experiences
+        self.add_section(title="Teaching Experiences")
+        for teach in Indrajit.teaching_experience:
+            teach_desc = (
+                "\n"
+                + r"\cvlistitem{"
+                + f"Course: {teach['course']}" 
+                + "}"
+                + "\n"
+                + r"\cvlistitem{Instructor: "
+                + teach['instructor']
+                + "}"
+            )
 
+            self.add_cventry(
+                years=teach['year'],
+                degree_or_job_title=teach['role'],
+                institution_or_employer=teach['institute'],
+                localization=teach['country'],
+                description=teach_desc
+            )
+
+
+        # Conferences
+        self.add_section(title="Conferences and Workshops Attended")
+        for conf in Indrajit.conferences:
+            if 'website' in conf and conf['website']:
+                conf_web = (
+                    "\n"
+                    + r"\cvlistitem{Website: "
+                    + _get_weblink_tex(url=conf['website']['url'], url_text=conf['website']['url_text'])
+                    + "}"
+                )
+            else:
+                conf_web = ''
+                
+
+            conf_desc = (
+                "\n"
+                + r"\cvlistitem{"
+                + f"Venue: {conf['venue']}" 
+                + "}"
+                + conf_web
+            )
+            
+            self.add_cventry(
+                years=conf['date'],
+                degree_or_job_title=conf['institute'],
+                institution_or_employer=conf['title'],
+                localization=conf['country'],
+                description=conf_desc
+            )
+
+        # References
+        self.add_section(title="References")
+        for ref in Indrajit.references:
+            _tex = (
+                "\n"
+                + r"\textbf{"
+                + ref['name']
+                + "}"
+                + ", "
+                + "\\emph{" + ref['designation'] + "}"
+                + " | "
+                + ref['address']
+                + " | "
+                + "\\texttt{" + ref['email'] + "}"
+            )
+            self.add_cvlistitem(_tex)
+        
 
 
 def main():
@@ -106,9 +192,7 @@ def main():
     indrajit = IndrajitCV()
 
     setup_tex_dir(tex_dir=tex_dir, cvObj=indrajit)
-    print(f"CV LaTeX written successfully at `{tex_dir}`")
-
-    print(indrajit.text)
+    print(f"\n\nCV LaTeX written successfully at `{tex_dir}`\n")
 
     # Change the dir
     os.chdir(str(tex_dir))
